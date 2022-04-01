5471e429e3f51cae3546cc67b385bdfc86be4eea
Varsha Boora <56237784+VarshaBoora@users.noreply.github.com>
2022-04-01 04:45:16 -0400

    Delete admin-service directory

--- admin-service/src/main/java/com/admin/service/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.admin.service.repository;
-
-import com.admin.service.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-    Course getByCourseID(String courseID);
-}
--- admin-service/src/main/java/com/admin/service/repository/GradeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.admin.service.repository;
-
-import com.admin.service.model.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
-}
